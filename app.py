6bb4185e29ce50295764d769e2cf9b27075c6cc5
iviieww <viwe4547@gmail.com>
2025-05-13 18:09:49 +0700

    add login page

--- app.py
+++ app.py
@@ -27,5 +27,10 @@ def help():
     
     return render_template('help.html')
 
+@app.route('/login')
+def login():
+    
+    return render_template('login.html')
+
 if __name__ =="__main__":
     app.run(debug=True)
